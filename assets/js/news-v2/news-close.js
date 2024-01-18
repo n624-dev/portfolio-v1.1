a4fdc95f81632929ab1be8579a757356814d7ea6
n624 <nobu02183@gmail.com>
2024-01-18 17:10:13 +0900

    Update

--- assets/js/news-v2/news-close.js
+++ assets/js/news-v2/news-close.js
@@ -3,6 +3,7 @@ function hideDiv() {
     if (window.history.length >= 1) {
         window.history.back();
     } else {
+        window.close();
         window.location.href = 'newslist.html';
     }
 }
