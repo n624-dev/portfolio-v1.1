d968ad3916acb50c50018e927d2c5b3afcab660a
n624 <nobu02183@gmail.com>
2024-02-06 21:12:17 +0900

    Update

--- assets/js/news-v2/news-list.js
+++ assets/js/news-v2/news-list.js
@@ -8,18 +8,13 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     fetch('./api/')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
+        .then(response => response.json())
         .then(data => {
+            console.log('JSONファイルの内容:');
             console.log(data);
-            // ここにJSONデータを処理するコードを追加
         })
         .catch(error => {
-            console.error('There was a problem with the fetch operation:', error);
+            console.error('エラー:', error);
         });
 
     const newsListElement = document.getElementById('newsList');
