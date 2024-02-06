bd425511158270bfd4af100802810650aff1e700
n624 <nobu02183@gmail.com>
2024-02-06 23:15:08 +0900

    Update

--- assets/js/news-v2/news-view.js
+++ assets/js/news-v2/news-view.js
@@ -22,10 +22,10 @@ document.addEventListener("DOMContentLoaded", function () {
                 const month = originalDate.getMonth() + 1; // 月は0から始まるため、1を加えます
                 const day = originalDate.getDate();
 
-                const formattedDate = `${year}年${month}月${day}日`;
+                const formattedDate = `${year}\u5e74${month}\u6708${day}\u65e5`;
 
                 // ID と HTML タグを保ったまま表示
-                outputElement.innerHTML = `<div class="outputindiv"><div><span class="output-span">ID: ${res.id}</span><span class="output-span">　公開日: ${formattedDate}</span></div><button onclick="hideDiv()" class="output-close"><img src="assets/img/close.svg" class="output-close-img"></img></button></div>${res.content}<button onclick="hideDiv()" class="output-close-end"><img src="assets/img/close.svg" class="output-close-img-end"></img>閉じる</button>`
+                outputElement.innerHTML = `<div class="outputindiv"><div><span class="output-span">ID: ${res.id}</span><span class="output-span">\u3000\u516c\u958b\u65e5: ${formattedDate}</span></div><button onclick="hideDiv()" class="output-close"><img src="assets/img/close.svg" class="output-close-img"></img></button></div>${res.content}<button onclick="hideDiv()" class="output-close-end"><img src="assets/img/close.svg" class="output-close-img-end"></img>閉じる</button>`
 
             })
             .catch((err) => {
