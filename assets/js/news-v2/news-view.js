86c3a523e833dc5ec44fc9b0ea752d1a76e03878
n624 <nobu02183@gmail.com>
2024-02-11 18:03:00 +0900

    Update

--- assets/js/news-v2/news-view.js
+++ assets/js/news-v2/news-view.js
@@ -19,11 +19,12 @@
                 const formattedDate = `${year}\u5e74${month}\u6708${day}\u65e5`;
 
                 // ID と HTML タグを保ったまま表示
-                outputElement.innerHTML = `<div class="outputindiv"><div><span class="output-span">ID: ${res.id}</span><span class="output-span">\u3000\u516c\u958b\u65e5: ${formattedDate}</span></div><button onclick="hideDiv()" class="output-close"><img src="assets/img/close.svg" class="output-close-img"></img></button></div>${res.content}<button onclick="hideDiv()" class="output-close-end"><img src="assets/img/close.svg" class="output-close-img-end"></img>閉じる</button>`
+                outputElement.innerHTML = `<div class="outputindiv"><div><span class="output-span">ID: ${data.id}</span><span class="output-span">\u3000\u516c\u958b\u65e5: ${formattedDate}</span></div><button onclick="hideDiv()" class="output-close"><img src="assets/img/close.svg" class="output-close-img"></img></button></div>${data.content}<button onclick="hideDiv()" class="output-close-end"><img src="assets/img/close.svg" class="output-close-img-end"></img>閉じる</button>`
 
             })
             .catch((error) => {
                 console.error(error);
+                alert("\u30a8\u30e9\u30fc\u304c\u767a\u751f\u3057\u307e\u3057\u305f\u3002\u000a\u3057\u3070\u3089\u304f\u5f85\u3063\u3066\u304b\u3089\u518d\u5ea6\u30a2\u30af\u30bb\u30b9\u3057\u3066\u304f\u3060\u3055\u3044\u3002")
                 window.location.href = 'newslist.html';
             });
     } else {
