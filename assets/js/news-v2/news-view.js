c6fd19ec61e8c7946717973fb31ad266ee712c51
n624 <nobu02183@gmail.com>
2024-02-11 18:05:44 +0900

    Update

--- assets/js/news-v2/news-view.js
+++ assets/js/news-v2/news-view.js
@@ -19,7 +19,7 @@
                 const formattedDate = `${year}\u5e74${month}\u6708${day}\u65e5`;
 
                 // ID と HTML タグを保ったまま表示
-                outputElement.innerHTML = `<div class="outputindiv"><div><span class="output-span">ID: ${data.id}</span><span class="output-span">\u3000\u516c\u958b\u65e5: ${formattedDate}</span></div><button onclick="hideDiv()" class="output-close"><img src="assets/img/close.svg" class="output-close-img"></img></button></div>${data.content}<button onclick="hideDiv()" class="output-close-end"><img src="assets/img/close.svg" class="output-close-img-end"></img>閉じる</button>`
+                outputElement.innerHTML = `<div class="outputindiv"><div><span class="output-span">ID: ${data.id}</span><span class="output-span">\u3000\u516c\u958b\u65e5: ${formattedDate}</span></div><button onclick="hideDiv()" class="output-close"><img src="assets/img/close.svg" class="output-close-img"></img></button></div>${data.content}<button onclick="hideDiv()" class="output-close-end"><img src="assets/img/close.svg" class="output-close-img-end"></img>\u9589\u3058\u308b</button>`
 
             })
             .catch((error) => {
