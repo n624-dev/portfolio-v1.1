e7a4f10f27b8cf07bb3633cbc0ea3f926504deda
n624 <nobu02183@gmail.com>
2024-02-16 20:47:30 +0900

    Update

--- assets/js/news-v2/news-oth.js
+++ assets/js/news-v2/news-oth.js
@@ -1,6 +1,6 @@
 function hideDiv() {
     console.log(history.length);
-    if (window.history.length >= 1) {
+    if (window.history.length > 1) {
         window.history.back();
     } else {
         const urlParams = new URLSearchParams(window.location.search);
