06742da5ba057db0c4c89c23f834834e9c13f800
n624 <nobu02183@gmail.com>
2024-02-28 21:01:26 +0900

    Update

---
+++ assets/js/news-v2/news-index.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+    fetch(`https://www.n624.net/api/news/?limit=5`)
+        .then(response => response.json())
+        .then(data => {
+            data.contents.forEach((item, index) => {
+                const titleElement = document.querySelector(`#title${index + 1}`);
+                if (titleElement) {
+                    categoryname = item.category.name;
+                    titlename = item.title;
+                    titleElement.textContent = "[" + categoryname + "]" + titlename;
+
+                    if (item.title.trim().length > 0) {
+                        titleElement.style.display = "list-item";
+                    }
+
+                    titleElement.addEventListener('click', () => {
+                        window.location.href = `/newsview?contents=${item.id}`;
+                    });
+                }
+            });
+        })
+        .catch((err) => console.error(err));
+});
